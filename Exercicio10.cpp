8faefa969fac0763632f3f53fd4fae167720a83c
HenriqueHanschStepic <38055978+HenriqueHanschStepic@users.noreply.github.com>
2018-04-16 21:42:44 -0300

    Update Exercicio10.cpp
    Incrementação do código

--- Exercicio10.cpp
+++ Exercicio10.cpp
@@ -1,1 +1,23 @@
+#include  <stdio.h>
 
+int main () {
+	int cont, j, k, n, numero;
+ 	
+	printf ("Insira um numero:\n");
+ 	scanf ("%d", &numero);
+ 	printf("\n");
+ 	n = 0;
+ 	for (cont=1; cont<=numero; cont++){
+  		for (j=numero; j<=cont; j++)
+		printf ("");
+  		for (k=1; k<=i; k++) {
+  			if (n < numero) {
+  				n++;
+				printf ("%d ", n);
+			}
+		}
+  		printf ("\n");
+ 	}
+	getchar ();
+	return 0;
+}
