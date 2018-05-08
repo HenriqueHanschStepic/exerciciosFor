5141ea164d323a22465bc5d85dc91db39d15f168
HenriqueHanschStepic <38055978+HenriqueHanschStepic@users.noreply.github.com>
2018-05-07 21:08:49 -0300

    Update Exercicio12.cpp

--- Exercicio12.cpp
+++ Exercicio12.cpp
@@ -6,16 +6,16 @@
 main(){
 	setlocale(LC_ALL, "Portuguese");
 
-	int fator1  = 0,
-		fator2   = 0,
-		produto = 0;
+	int cont  = 0,
+	    cont2   = 0,
+	    resultado = 0;
 			
-	for (fator1 = 1; fator1 <= 10; fator1++) {
-		printf("Tabuado do %d\n", fator1);
-		for (fator2 = 1; fator2 <= 10; fator2++) {
-			produto = fator1 * fator2;
+	for (cont = 1; cont <= 10; cont++) {
+		printf("Tabuado do %d\n", cont);
+		for (cont2 = 1; cont2 <= 10; cont2++) {
+			resultado = cont * cont2;
 			
-			printf("%d * %d = %d\n", fator1, fator2, produto);
+			printf("%d * %d = %d\n", cont, cont2, resultado);
 		}
 		printf("\n");
     }
